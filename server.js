c6d168801064578f4a2347f42b7441a71aaecfde
Santhosh Kumar Sellavel <skrece22@gmail.com>
2018-01-08 11:07:50 +0530

    Setup projects page

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + "/public"));
 
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
     res.render('about.hbs',{
         pageTitle: 'About Page'
     })
+});
+
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Portfolio Page",
+    message:"This is a sample node project using node, express & handlebarJs."
+  });
 });
 
 app.get('/bad', (req, res) => {
